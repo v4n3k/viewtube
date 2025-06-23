2fcae6d183a14199c5895753e44591685e05725b
v4n3k <ivan71012@icloud.com>
2025-06-23 18:31:37 +0300

    feat(design-system): Introduce CSS custom properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import { Roboto } from 'next/font/google';
+import 'src/app/styles/globals.css';
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
+const roboto = Roboto({
+	weight: ['400', '500', '600', '700'],
 	subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
+	variable: '--font-family',
+	display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -24,9 +21,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>
-				{children}
-			</body>
+			<body className={roboto.variable}>{children}</body>
 		</html>
 	);
 }
